9f8b0dcc1804f06046b750d7b2838af4110d5e71
Ramsis <ramsis@manuloewe.de>
2016-09-30 18:13:33 +0200

    Adapt build script for WLA DX v9.7b

--- src/makeall.bat
+++ src/makeall.bat
@@ -21,8 +21,8 @@ smconv -s -o soundbnk "music\parforceritt_b2_downsampled.it"
 echo -- assembling BOOTROM.SFC/UPDATE.ROM --
 echo [objects] > bootrom.lnk
 echo bootrom.o >> bootrom.lnk
-wla-65816 -xo bootrom.asm bootrom.o
-wlalink -rs bootrom.lnk bootrom.sfc
+wla-65816 -x -o bootrom.o bootrom.asm
+wlalink -r -s bootrom.lnk bootrom.sfc
 copy bootrom.sfc out\bootrom.sfc
 copy bootrom.sfc out\POWERPAK\UPDATE.ROM
 del bootrom.o
